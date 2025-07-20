99019731c5d924150bb0411d0a6d9342220bb0fd
TLarsh <adeyemisam2015@gmail.com>
2025-07-20 10:47:19 +0100

    update urls

--- core/urls.py
+++ core/urls.py
@@ -55,19 +55,19 @@ urlpatterns = [
     path('api/request-reset/', RequestOTPView.as_view()),
     path('api/verify-otp/', VerifyOTPView.as_view()),
     path('api/reset-password/', ResetPasswordView.as_view()),
-    # path('api/dates/', DateListView.as_view(), name='dates'),       # Ensure 'dates' matches test
+    path('api/dates/', DateListView.as_view(), name='dates'),       # Ensure 'dates' matches test
     # path('api/blackout-dates/', BlackoutDatesView.as_view(), name='blackout_dates'),
-    # path('api/profile/', ProfileView.as_view(), name='profile'),
+    path('api/profile/', ProfileView.as_view(), name='profile'),
     # path('api/update-wellbeing/', UpdateWellbeingView.as_view(), name='update_wellbeing'),
     # path('api/log-action/', LogActionView.as_view(), name='log_action'),
     # path('api/update-profile/', UpdateProfileView.as_view(), name='update_profile'),
     # path('api/update-settings/', UpdateSettingsView.as_view(), name='update_settings'),
-    # path('api/settings/', GetSettingsView.as_view(), name='get_settings'),
+    path('api/settings/', GetSettingsView.as_view(), name='get_settings'),
     # path('api/onboarding/', GetOnboardingDataView.as_view(), name='get_onboarding_data'),
     # path('api/update-onboarding/', UpdateOnboardingDataView.as_view(), name='update_onboarding_data'),
     # path("fetch-public-holidays/", FetchPublicHolidaysView.as_view(), name="fetch-public-holidays"),
     # path("list-public-holidays/", ListPublicHolidaysView.as_view(), name="list-public-holidays"),
-    # path("optimization-score/", OptimizationScoreView.as_view(), name="optimization-score"),
+    path("optimization-score/", OptimizationScoreView.as_view(), name="optimization-score"),
     # path("gamification-data/", GamificationDataView.as_view(), name="gamification-data"),
     # path("suggested-dates/", SuggestedDatesView.as_view(), name="suggested-dates"),
 
